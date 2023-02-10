9863ce51d1b3abacac300d2f07dbabf8de8ac832
houserTyler <lyl763092@sina.com>
2023-02-11 01:42:57 +0800

    08 - Tailwind CSS with Shopify Theme CLI Store 2.0 and Webpack

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    './assets/*.liquid',
+    './config/*.json',
+    './layout/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+    './templates/*.liquid',
+    './templates/*.json',
+    './templates/customers/*.json',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
